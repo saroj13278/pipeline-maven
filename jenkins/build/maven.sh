40b381950969f9552db8c872ccef8fa99ab1d1f3
root <root@ip-172-31-87-93.ec2.internal>
2024-07-20 16:45:26 +0000

    adding

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -7,3 +7,11 @@ WORKSPACE=/var/lib/jenkins/workspace/maven-app
 
 #docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app/ maven "$@"
 docker run --rm -i -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
+
+
+# List the contents of the target directory for debugging
+echo "Listing contents of /app/ directory:"
+docker run --rm -i -v $WORKSPACE/java-app:/app -w /app/ maven ls -la /app/
+
+echo "Listing contents of /app/target/ directory:"
+docker run --rm -i -v $WORKSPACE/java-app:/app -w /app/ maven ls -la /app/target/
