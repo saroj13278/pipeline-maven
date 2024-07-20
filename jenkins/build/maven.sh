d02d87f849ecc7c7480f614e1d1dbc578856502e
root <root@ip-172-31-87-93.ec2.internal>
2024-07-20 15:15:18 +0000

    modified build script

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -5,4 +5,5 @@ echo "******************************************"
 
 WORKSPACE=/var/lib/jenkins/workspace/maven-app
 
-docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app/ maven "$@"
+#docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app/ maven "$@"
+docker run --rm -i -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app/ maven "$@"
