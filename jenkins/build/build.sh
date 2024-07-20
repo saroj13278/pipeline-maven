10d0c788485e1728bd4be6208316e9ce51e1011d
root <root@ip-172-31-87-93.ec2.internal>
2024-07-20 16:51:36 +0000

    adding

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,6 +4,6 @@
 #copy the jar
 WORKSPACE=/var/lib/jenkins/workspace/maven-app
 ls -la $WORKSPACE/java-app/target/
-cp -f $WORKSPACE/java-app/target/*.jar jenkins/build/
+cp -f /var/lib/jenkins/workspace/maven-app/java-app/target/*.jar jenkins/build/
 
 cd jenkins/build/ && docker-compose -f docker-compose-build.yml build
