276b3a29773bf7daad24688af96beae611711826
root <root@ip-172-31-87-93.ec2.internal>
2024-07-21 09:30:27 +0000

    commiting

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -5,8 +5,12 @@ echo "******************************************"
 
 WORKSPACE=/var/lib/jenkins/workspace/maven-app
 
+# Get Jenkins user and group ID
+USER_ID=$(id -u jenkins)
+GROUP_ID=$(id -g jenkins)
+
 #docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app/ maven "$@"
-docker run --rm -i -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
+docker run --rm -i $USER_ID:$GROUP_ID -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
 
 
 # List the contents of the target directory for debugging
